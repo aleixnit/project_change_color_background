e64d9336ee7605e9211da5bc7a780ad371c6e268
aleixnit <nitjac210@gmail.com>
2023-04-20 11:11:45 +0200

    exercice cambios de color de fondo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
 //En este array de strings, tenemos todos los colores
 const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
 
+// Al hacer click en el botón "Click Me!" el color del fondo de body siemore cambia al mismo color, por ejemplo, amarillo. Podeis modificar el html si lo creeis oportuno.
 
+// Paso 1: Poner una oreja al botón para que capture el evento click. Que me muestre por consola 'me has hecho click'
+document.querySelector("#btn").onclick = function () {
+    console.log("Me has hecho click");
+};
+
+// Paso 2: cambia el color del body a amarillo: 1 linea de codigo.
+// Seleccionar el nodo 'body' y cambiar su estilo en linea a amarillo. Recuerda que la 'style' es un objeto con muchas propiedades.
+
+
+
+
+
+
+
+
+  function setRandomBackgroundColor() {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    const randomColor = colors[randomIndex];
+    document.body.style.backgroundColor = randomColor;
+  }
+
+  const btn = document.getElementById('btn');
+  btn.addEventListener('click', setRandomBackgroundColor);
+
+  // Llama a la función en la carga inicial de la página
+  setRandomBackgroundColor();
